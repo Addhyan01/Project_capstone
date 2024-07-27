6f7d6924405e8e1f59049a4e33ae0452e90fbeae
Addhyan01 <addhyankumar@gmail.com>
2024-07-27 21:19:26 +0530

    first commit

---
+++ client/src/page/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './NotFoundPage.module.css'
+
+export const NotFoundPage = () => {
+  return (
+    <div className={styles.container}>
+        <h1>404 Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  )
+}
